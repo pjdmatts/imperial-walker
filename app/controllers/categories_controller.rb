8cba760af6909912ef8d68ec557e54e50d98cc13
Peter Matthews <pjdmatthews7@gmail.com>
2015-10-13 06:29:12 -0400

    Category Form Working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,13 +4,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+  	@category = Category.new
   end
 
   def create
-  	@category = Category.new(params[:category])
- 
+  	@category = Category.new(category_params)
   	@category.save
   	redirect_to categories_path
+  end
+
+  def destroy
+  	@category = Category.find(params[:id])
+  	@category.destroy
+  	redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+  	params.require(:category).permit(:name)
   end
 
 end
