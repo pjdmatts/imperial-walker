cc35b9a15382a84f189f6885ba0ffaeb3d33a424
Peter Matthews <pjdmatthews7@gmail.com>
2015-10-18 13:06:54 -0400

    Style New Product Form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,31 @@
 class ProductsController < ApplicationController
 	def index
-	end
+  	@products = Product.all
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+  	@product.save
+  	redirect_to products_path
+  end
+
+  def destroy
+  	@product = Product.find(params[:id])
+  	@product.destroy
+  	redirect_to products_path
+  end
+
+  private
+
+  def product_params
+  	params.require(:product).permit(:name, :price, :category_id)
+  end
 end
